14ebee68c46ec0a52eb06dacbcedc1044f367809
jjs-cloud <277815495+jjs-cloud@users.noreply.github.com>
2026-05-10 09:57:45 -0500

    make log dir if it doesn't exist

--- logger.py
+++ logger.py
@@ -86,6 +86,7 @@ def _write_debug_log(msg: str) -> None:
     if not enabled or not path:
         return
     try:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, "a", encoding="utf-8") as f:
             f.write(msg + "\n")
     except Exception:
